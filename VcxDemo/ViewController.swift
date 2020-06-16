1c3d82d8fa10466d16ed8db785597c30eba5a6b7
Thomas Kim <jeong.kim@sk.com>
2020-06-16 10:16:12 +0900

    Created a default single view app

---
+++ VcxDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VcxDemo
+//
+//  Created by Jeong Kim on 16/06/2020.
+//  Copyright © 2020 SK Telecom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
