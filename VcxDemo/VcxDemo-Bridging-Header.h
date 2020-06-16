81fef512577fb20a5b3702257d7e35e66734c0da
Thomas Kim <jeong.kim@sk.com>
2020-06-16 10:45:15 +0900

    Created an Objective-C bridging header file to expose Objective-C framework in the swift project

---
+++ VcxDemo/VcxDemo-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  VcxDemo-Bridging-Header.h
+//  VcxDemo
+//
+//  Created by Jeong Kim on 16/06/2020.
+//  Copyright © 2020 SK Telecom. All rights reserved.
+//
+
+#ifndef VcxDemo_Bridging_Header_h
+#define VcxDemo_Bridging_Header_h
+
+#import <vcx/vcx.h>
+
+#endif /* VcxDemo_Bridging_Header_h */
