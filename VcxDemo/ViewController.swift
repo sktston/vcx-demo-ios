a2e5f93d407849a56ca22d16b0fdbe7386592542
Thomas Kim <jeong.kim@sk.com>
2020-07-23 16:07:14 +0900

    Use the protocol version 4

--- VcxDemo/ViewController.swift
+++ VcxDemo/ViewController.swift
@@ -33,7 +33,7 @@ class ViewController: UIViewController {
               "wallet_key": "123",
               "payment_method": "null",
               "enterprise_seed": "000000000000000000000000Trustee1",
-              "protocol_type": "3.0"
+              "protocol_type": "4.0"
             }
             """
 
